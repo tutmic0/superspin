a9fd8dea319cb97f189e33be8dfd2c3c7d4f326b
tutmic0 <tvoj@email.com>
2026-03-09 00:13:08 +0100

    fix: static export for cloudflare

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'edge',
-  },
+  output: 'export',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
